a924f356d10d699df1ff1f7229cd53fd2a784da9
sebastian <sebass810@hotmail.com>
2019-05-31 16:08:14 -0300

    personas autorizadas

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -15,26 +15,20 @@ class ReportController < ApplicationController
   # end
 
 	  def estadisticas_bajadas
-	  	if params["fecha_desde"].present? and params["fecha_hasta"].present?
-	  		if params["fecha_desde"] > params["fecha_hasta"]
-	  			redirect_to '/report/index'
-	  		else
-					@bajadas_client = DescentClient.all
-					if params["fecha_desde"].present?
-						@bajadas_client = @bajadas_client.where('fecha >= ?', params["fecha_desde"])
-					end
-					if params["fecha_hasta"].present?
-						@bajadas_client = @bajadas_client.where('fecha <= ?', params["fecha_hasta"])
-					end
-					respond_to do |format|
-						format.html
-						format.pdf do 
-							pdf = BajadasPdf.new(@bajadas, @bajadas_client)
-							send_data pdf.render, filename: 'estadisticasEstados.pdf', type: 'application/pdf', disposition: "inline"
-						end
-					end
+			@bajadas_client = DescentClient.all
+			if params["fecha_desde"].present?
+				@bajadas_client = @bajadas_client.where('fecha >= ?', params["fecha_desde"])
+			end
+			if params["fecha_hasta"].present?
+				@bajadas_client = @bajadas_client.where('fecha <= ?', params["fecha_hasta"])
+			end
+			respond_to do |format|
+				format.html
+				format.pdf do 
+					pdf = BajadasPdf.new(@bajadas, @bajadas_client)
+					send_data pdf.render, filename: 'estadisticasEstados.pdf', type: 'application/pdf', disposition: "inline"
 				end
-	  	end
+			end
 		end
 
 end
